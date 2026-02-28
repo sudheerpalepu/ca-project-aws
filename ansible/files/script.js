25c0b3e0357eb0e3e49398a4fd910271f4285489
sudheerpalepu <146517155+sudheerpalepu@users.noreply.github.com>
2026-02-28 03:43:12 +0000

    update7

--- ansible/files/script.js
+++ ansible/files/script.js
@@ -1,5 +1,5 @@
 document.getElementById("clickMe").addEventListener("click", function() {
     const msg = document.getElementById("msg");
-    msg.textContent = "Thanks for clicking the button!";
+    msg.textContent = "Thanks for clicking the button ok!";
     msg.style.color = "green";
 });
